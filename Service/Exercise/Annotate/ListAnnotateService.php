00aa82818342689adec93fbcf0554d801ebe51c7
Lucas Foulon <lucas.foulon@etu.univ-lyon1.fr>
2015-08-24 13:32:42 +0200

    Ajout de la classe Service/Exercise/Annotate/ListAnnotateService
    	nouveau fichier: Service/Exercise/Annotate/ListAnnotateService.php

---
+++ Service/Exercise/Annotate/ListAnnotateService.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arabesque
+ * Date: 24/08/15
+ * Time: 13:29
+ */
+
+
+namespace SimpleIT\ClaireExerciseBundle\Service\Exercise\Annotate;
+
+use SimpleIT\ClaireExerciseBundle\Entity\Annotate\ListAnnotate;
+use SimpleIT\ClaireExerciseBundle\Entity\ExerciseResource\ExerciseResource;
+use SimpleIT\ClaireExerciseBundle\Repository\Exercise\Annotate\ListAnnotateRepository;
+use SimpleIT\ClaireExerciseBundle\Service\Exercise\ExerciseResource\ExerciseResourceService;
+use SimpleIT\ClaireExerciseBundle\Service\Exercise\ExerciseResource\ExerciseResourceServiceInterface;
+use SimpleIT\ClaireExerciseBundle\Service\TransactionalService;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class ListAnnotateService extends TransactionalService
+{
+    /**
+     * @var ListAnnotateRepository
+     */
+    protected $listAnnotateRepository;
+
+    /**
+     * @var ExerciseResourceService
+     */
+    protected $resourceService;
+
+    /**
+     * Set listAnnotateRepository
+     *
+     * @param ListAnnotateRepository $listAnnotateRepository
+     */
+    public function setListAnnotateRepository($listAnnotateRepository)
+    {
+        $this->listAnnotateRepository = $listAnnotateRepository;
+    }
+
+    /**
+     * Set resource service
+     *
+     * @param ExerciseResourceServiceInterface $resourceService
+     */
+    public function setResourceService($resourceService)
+    {
+        $this->resourceService = $resourceService;
+    }
+
+
+    /**
+     * Get all the list annotate
+     *
+     * @param CollectionInformation $collectionInformation
+     * @param int                   $entityId
+     * @param int                   $userId
+     *
+     * @throws AccessDeniedException
+     * @return array
+     */
+    public function getAll(
+        $entityId = null,
+        $userId = null
+    )
+    {
+        $entity = null;
+        if (!is_null($entityId)) {
+            $entity = $this->resourceService->get($entityId);
+            if (!$entity->getPublic() && $entity->getOwner()->getId() !== $userId) {
+                throw new AccessDeniedException();
+            }
+        }
+
+        return $this->listAnnotateRepository->findAllBy(
+            $entity
+        );
+    }
+
+    /**
+     * Delete all the list annotate for an owner resource
+     *
+     * @param ExerciseResource $resource
+     */
+    public function deleteAllByEntity($resource)
+    {
+        $this->listAnnotateRepository->deleteAllByEntity($resource);
+        $this->em->flush();
+    }
+
+}
