2cbf107117397a9895d6bca3d31516ea1cdf3d22
Lucas Foulon <lucas.foulon@etu.univ-lyon1.fr>
2015-08-27 19:19:49 +0200

    Ajout de la classe MetadataRepository
    	nouveau fichier: Repository/Exercise/Annotate/MetadataRepository.php

---
+++ Repository/Exercise/Annotate/MetadataRepository.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * This file is part of CLAIRE.
+ *
+ * CLAIRE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CLAIRE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CLAIRE. If not, see <http://www.gnu.org/licenses/>
+ */
+
+namespace SimpleIT\ClaireExerciseBundle\Repository\Exercise\Annotate;
+
+use SimpleIT\ClaireExerciseBundle\Repository\Exercise\SharedEntity\SharedMetadataRepository;
+
+/**
+ * List Annotate Metadata repository
+ *
+ * @author Baptiste Cablé <baptiste.cable@liris.cnrs.fr>
+ */
+class MetadataRepository extends SharedMetadataRepository
+{
+    const METADATA_TABLE = 'claire_exercise_resource_list_annotate_metadata';
+
+    const ENTITY_ID_FIELD_NAME = 'list_annotate_id';
+
+    const ENTITY_NAME = 'list_annotate';
+
+}
