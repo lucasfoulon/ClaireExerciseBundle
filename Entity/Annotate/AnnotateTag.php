eda010c2b4b008329bf5b4aeb31d212af4dc6aa4
Lucas Foulon <lucas.foulon@etu.univ-lyon1.fr>
2015-08-30 13:34:11 +0200

    Ajout de la classe et table AnnotateTag
    	modifié:         Entity/Annotate/Annotate.php
    	nouveau fichier: Entity/Annotate/AnnotateTag.php
    	nouveau fichier: Resources/config/doctrine/Annotate.AnnotateTag.orm.xml

--- Entity/Annotate/AnnotateTag.php
+++ Entity/Annotate/AnnotateTag.php
@@ -2,16 +2,15 @@
 /**
  * Created by PhpStorm.
  * User: arabesque
- * Date: 29/07/15
- * Time: 12:02
+ * Date: 30/08/15
+ * Time: 12:57
  */
-namespace SimpleIT\ClaireExerciseBundle\Entity\Annotate;
 
+namespace SimpleIT\ClaireExerciseBundle\Entity\Annotate;
 
 use SimpleIT\ClaireExerciseBundle\Entity\ExerciseResource\ExerciseResource;
 
-class Annotate
-{
+class AnnotateTag {
 
     /**
      * @var ListAnnotate
@@ -24,19 +23,19 @@ class Annotate
     private $resource;
 
     /**
+     * @var Annotate
+     */
+    private $annotate;
+
+    /**
+     * @var string
+     */
+    private $key;
+
+    /**
      * @var string
      */
     private $value;
-
-    /**
-     * @var int
-     */
-    private $start;
-
-    /**
-     * @var int
-     */
-    private $end;
 
     /**
      * Set list_annotate
@@ -78,6 +77,46 @@ class Annotate
         return $this->resource;
     }
 
+    /*
+     * Set annotate
+     *
+     * @param Annotate $annotate
+     */
+    public function setAnnotate($annotate)
+    {
+        $this->annotate = $annotate;
+    }
+
+    /**
+     * Get annotate
+     *
+     * @return Annotate
+     */
+    public function getAnnotate()
+    {
+        return $this->annotate;
+    }
+
+    /**
+     * Get key
+     *
+     * @return string
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * Set key
+     *
+     * @param string $key
+     */
+    public function setKey($key)
+    {
+        $this->key = $key;
+    }
+
     /**
      * Get value
      *
@@ -98,43 +137,4 @@ class Annotate
         $this->value = $value;
     }
 
-    /**
-     * Get start
-     *
-     * @return int
-     */
-    public function getStart()
-    {
-        return $this->start;
-    }
-
-    /**
-     * Set start
-     *
-     * @param int $start
-     */
-    public function setStart($start)
-    {
-        $this->start = $start;
-    }
-
-    /**
-     * Get end
-     *
-     * @return int
-     */
-    public function getEnd()
-    {
-        return $this->end;
-    }
-
-    /**
-     * Set end
-     *
-     * @param int $end
-     */
-    public function setEnd($end)
-    {
-        $this->end = $end;
-    }
 }
