d6525a96a4a5609abc3867f05d1400b2711c3e72
Lucas Foulon <lucas.foulon@etu.univ-lyon1.fr>
2015-09-08 16:54:10 +0200

    Ajout de TextExerciseService (class)
    	nouveau fichier: Service/Exercise/ExerciseCreation/TextExerciseService.php

---
+++ Service/Exercise/ExerciseCreation/TextExerciseService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arabesque
+ * Date: 08/09/15
+ * Time: 15:37
+ */
+
+namespace SimpleIT\ClaireExerciseBundle\Service\Exercise\ExerciseCreation;
+
+
+class TextExerciseService
+{
+
+}
