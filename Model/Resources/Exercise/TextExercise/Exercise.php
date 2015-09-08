9d796da3acdf92188abf9ddfea20b3ce3bc55a40
Lucas Foulon <lucas.foulon@etu.univ-lyon1.fr>
2015-09-08 16:31:45 +0200

    La classe Exercise un peu etoffé
    	modifié:         Model/Resources/Exercise/TextExercise/Exercise.php

--- Model/Resources/Exercise/TextExercise/Exercise.php
+++ Model/Resources/Exercise/TextExercise/Exercise.php
@@ -16,5 +16,60 @@ use SimpleIT\ClaireExerciseBundle\Model\Resources\Exercise\Common\CommonExercise
  */
 class Exercise extends CommonExercise
 {
+    /**
+     * @var array $annotates An array of Annotate
+     * @Serializer\Exclude
+     */
+    private $annotates = array();
+
+    /**
+     * Get annotate at index $key
+     *
+     * @param int $key The index
+     *
+     * @return Annotate The annotate
+     */
+    public function getAnnotate($key)
+    {
+        return $this->annotates[$key];
+    }
+
+    /**
+     * Get annotates
+     *
+     * @return array An array of ExerciseAnnotate
+     */
+    public function getAnnotates()
+    {
+        return $this->annotates;
+    }
+
+    /**
+     * Add a annotate
+     *
+     * @param Annotate $annotate
+     */
+    public function addAnnotate(Annotate $annotate)
+    {
+        $this->annotates[] = $annotate;
+    }
+
+    /**
+     * Get the number of annotates
+     *
+     * @return int The number of annotates
+     */
+    public function getNumberOfAnnotates()
+    {
+        return count($this->annotates);
+    }
+
+    /**
+     * Compute the itemCount
+     */
+    public function finalize()
+    {
+        $this->itemCount = count($this->annotates);
+    }
 
 }
