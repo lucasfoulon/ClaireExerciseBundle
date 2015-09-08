b56c5f2fbb1dd0b2e6454405df0492a53180c53f
Lucas Foulon <lucas.foulon@etu.univ-lyon1.fr>
2015-09-08 11:48:51 +0200

    Ajout fichier TextExercise
    	modifié:         Model/Resources/ExerciseModel/TextExercise/Model.php
    	modifié:         Model/Resources/ExerciseModel/TextExercise/TextBlock.php

--- Model/Resources/ExerciseModel/TextExercise/Model.php
+++ Model/Resources/ExerciseModel/TextExercise/Model.php
@@ -1,12 +1,83 @@
 <?php
-
 /**
  * Created by PhpStorm.
  * User: arabesque
- * Date: 01/09/15
- * Time: 13:11
+ * Date: 30/07/15
+ * Time: 10:46
  */
-class Model
-{
 
+namespace SimpleIT\ClaireExerciseBundle\Model\Resources\ExerciseModel\TextExercise;
+
+use JMS\Serializer\Annotation as Serializer;
+use SimpleIT\ClaireExerciseBundle\Model\Resources\ExerciseModel\Common\CommonModel;
+use SimpleIT\ClaireExerciseBundle\Model\Resources\ExerciseObject\ExerciseTextObject;
+
+/**
+ *
+ */
+class Model extends CommonModel
+{
+    /**
+     * @var array $textBlocks An array of TextBlock
+     * @Serializer\Type("array<SimpleIT\ClaireExerciseBundle\Model\Resources\ExerciseModel\TextExercise\TextBlock>")
+     * @Serializer\Groups({"details", "exercise_model_storage"})
+     */
+    private $textBlocks = array();
+
+    /**
+     * @var boolean $shuffleQuestionsOrder True if the questions have to be shuffled
+     * @Serializer\Type("boolean")
+     * @Serializer\Groups({"details", "exercise_model_storage"})
+     */
+    private $shuffleQuestionsOrder;
+
+    /**
+     * Get the text blocks
+     *
+     * @return array An array of TextBlock
+     */
+    public function getTextBlocks()
+    {
+        return $this->textBlocks;
+    }
+
+    /**
+     * Set text blocks
+     *
+     * @param array $textBlocks An array of TextBlock
+     */
+    public function setTextBlocks($textBlocks)
+    {
+        $this->textBlocks = $textBlocks;
+    }
+
+    /**
+     * Add a text block
+     *
+     * @param ExerciseTextObject $textBlock
+     */
+    public function addTextBlock(ExerciseTextObject $textBlock)
+    {
+        $this->textBlocks[] = $textBlock;
+    }
+
+    /**
+     * Get shuffleQuestionsOrder
+     *
+     * @return boolean
+     */
+    public function isShuffleQuestionsOrder()
+    {
+        return $this->shuffleQuestionsOrder;
+    }
+
+    /**
+     * Set shuffleQuestionsOrder
+     *
+     * @param boolean $shuffleQuestionsOrder
+     */
+    public function setShuffleQuestionsOrder($shuffleQuestionsOrder)
+    {
+        $this->shuffleQuestionsOrder = $shuffleQuestionsOrder;
+    }
 }
--- Model/Resources/ExerciseModel/TextExercise/TextBlock.php
+++ Model/Resources/ExerciseModel/TextExercise/TextBlock.php
@@ -1,15 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: arabesque
- * Date: 01/09/15
- * Time: 13:11
+/*
+ * This file is part of CLAIRE.
+ *
+ * CLAIRE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CLAIRE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CLAIRE. If not, see <http://www.gnu.org/licenses/>
  */
 
 namespace SimpleIT\ClaireExerciseBundle\Model\Resources\ExerciseModel\TextExercise;
 
+use JMS\Serializer\Annotation as Serializer;
+use SimpleIT\ClaireExerciseBundle\Model\Resources\ExerciseModel\Common\ResourceBlock;
 
-class TextBlock
+/**
+ * Block of text
+ */
+class TextBlock extends ResourceBlock
 {
-
+    /**
+     * TextBlock constructor
+     *
+     * @param int $numberOfOccurrences
+     *
+     * @return TextBlock
+     */
+    function __construct(
+        $numberOfOccurrences
+    )
+    {
+        $this->numberOfOccurrences = $numberOfOccurrences;
+    }
 }
