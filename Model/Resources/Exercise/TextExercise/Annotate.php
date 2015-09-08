53370855307f4754d8c88b162dea8dd981660798
Lucas Foulon <lucas.foulon@etu.univ-lyon1.fr>
2015-09-08 16:20:26 +0200

    Ajout de deux classe pour la génération d'un exercice sur texte
    	nouveau fichier: Model/Resources/Exercise/TextExercise/Annotate.php
    	nouveau fichier: Model/Resources/Exercise/TextExercise/Exercise.php

---
+++ Model/Resources/Exercise/TextExercise/Annotate.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arabesque
+ * Date: 08/09/15
+ * Time: 16:18
+ */
+
+namespace SimpleIT\ClaireExerciseBundle\Model\Resources\Exercise\TextExercise;
+
+use JMS\Serializer\Annotation as Serializer;
+use SimpleIT\ClaireExerciseBundle\Model\Resources\Exercise\Common\CommonItem;
+
+class Annotate extends CommonItem
+{
+
+}
---
+++ Model/Resources/Exercise/TextExercise/Exercise.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arabesque
+ * Date: 08/09/15
+ * Time: 16:17
+ */
+
+namespace SimpleIT\ClaireExerciseBundle\Model\Resources\Exercise\TextExercise;
+
+use JMS\Serializer\Annotation as Serializer;
+use SimpleIT\ClaireExerciseBundle\Model\Resources\Exercise\Common\CommonExercise;
+
+/**
+ * Class Exercise
+ */
+class Exercise extends CommonExercise
+{
+
+}
