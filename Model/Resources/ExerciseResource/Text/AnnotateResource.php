abfea40485e39da592c502c7275155c7c7934fb1
Lucas Foulon <lucas.foulon@etu.univ-lyon1.fr>
2015-09-12 22:30:36 +0200

    Mes nvx fichiers
    	nouveau fichier: Model/Resources/ExerciseResource/Text/AnnotateResource.php
    	nouveau fichier: Model/Resources/ExerciseResource/Text/ListAnnotateResource.php
    	nouveau fichier: Resources/public/frontend/partials/learner/partial-text-exercise.html

---
+++ Model/Resources/ExerciseResource/Text/AnnotateResource.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: arabesque
+ * Date: 12/09/15
+ * Time: 21:33
+ */
+class AnnotateResource
+{
+
+}
---
+++ Model/Resources/ExerciseResource/Text/ListAnnotateResource.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arabesque
+ * Date: 12/09/15
+ * Time: 22:13
+ */
+
+namespace SimpleIT\ClaireExerciseBundle\Model\Resources\ExerciseResource\Text;
+
+
+class ListAnnotateResource
+{
+
+}
